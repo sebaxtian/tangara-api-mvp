4eea723160008c1b7d6d24217f2e9c5b64056a02
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2023-05-28 13:05:05 -0700

    Initial tests Lugares, done

---
+++ app/tests/test_lugares.py
@@ -0,0 +1,34 @@
+from fastapi.testclient import TestClient
+from fastapi import status
+import pandas as pd
+import io
+
+from app.main import app, get_db
+from app.dependencies.testing_database import override_get_db
+from app.tests.conftest import Codes
+
+
+app.dependency_overrides[get_db] = override_get_db
+client = TestClient(app)
+
+
+def test_get_lugares(tangaras):
+    response1 = client.get("/lugares/")
+    lugares = response1.json()
+
+    df_lugares = pd.DataFrame(data=lugares)
+    ids_lugares = df_lugares['id'].astype('int').values
+    # print(ids_lugares)
+
+    response2 = client.get("/lugares/?format=csv")
+    lugares_csv = response2.content
+    df_lugares_csv = pd.read_csv(io.BytesIO(lugares_csv))
+    ids_lugares_csv = df_lugares_csv['id'].astype('int').values
+    # print(ids_lugares_csv)
+
+    codes = [Codes.COMUNA, Codes.BARRIO, Codes.VEREDA, Codes.SECTOR, Codes.AREAEXP, Codes.AREAPRO]
+
+    assert response1.status_code == status.HTTP_200_OK
+    assert response2.status_code == status.HTTP_200_OK
+    assert all(code in ids_lugares for code in codes) == True
+    assert all(code in ids_lugares_csv for code in codes) == True
