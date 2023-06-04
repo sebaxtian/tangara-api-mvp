944bdf00516a3cf35bd204a3820bc8197309a427
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2023-06-03 20:06:03 -0700

    Initial endpoint get/realtime Tangaras data, Done

--- app/routers/pm25.py
+++ app/routers/pm25.py
@@ -3,8 +3,7 @@ from sqlalchemy.orm import Session
 from enum import IntEnum
 
 from app.dependencies.database import get_db
-from app.schemas.lugares import LugaresSchema
-from app.schemas.tangara import TangaraSchemaList
+from app.schemas.tangara import TangaraSchema
 from app.schemas.pm25 import PM25Schema
 from app.crud.comuna import ComunaCRUD
 from app.crud.barrio import BarrioCRUD
@@ -12,10 +11,11 @@ from app.crud.vereda import VeredaCRUD
 from app.crud.sector import SectorCRUD
 from app.crud.areaexp import AreaExpCRUD
 from app.crud.areapro import AreaProCRUD
+from app.utils.pm25 import pm25_realtime
 
 
 # IDs Lugares
-class Codes(IntEnum):
+class Codes(IntEnum): #TODO: Refactoring
     COMUNA = 1000
     BARRIO = 2000
     VEREDA = 3000
@@ -32,26 +32,27 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=PM25Schema, status_code=status.HTTP_200_OK)
-async def realtime(lugar: LugaresSchema, db: Session = Depends(get_db)) -> PM25Schema:
+@router.get("/{id}", response_model=PM25Schema, status_code=status.HTTP_200_OK)
+async def realtime(id: int, db: Session = Depends(get_db)) -> PM25Schema:
     
-    tangaras = []
+    tangaras: list[TangaraSchema] = []
 
-    if lugar.id in range(Codes.COMUNA, Codes.BARRIO):
-        tangaras = ComunaCRUD.read_tangaras(db, id_comuna=lugar.id)
-    if lugar.id in range(Codes.BARRIO, Codes.VEREDA):
-        tangaras = BarrioCRUD.read_tangaras(db, id_barrio=lugar.id)
-    if lugar.id in range(Codes.VEREDA, Codes.SECTOR):
-        tangaras = VeredaCRUD.read_tangaras(db, id_vereda=lugar.id)
-    if lugar.id in range(Codes.SECTOR, Codes.AREAEXP):
-        tangaras = SectorCRUD.read_tangaras(db, id_sector=lugar.id)
-    if lugar.id in range(Codes.AREAEXP, Codes.AREAPRO):
-        tangaras = AreaExpCRUD.read_tangaras(db, id_areaexp=lugar.id)
-    if lugar.id in range(Codes.AREAPRO, Codes.AREAPRO + 1000):
-        tangaras = AreaProCRUD.read_tangaras(db, id_areapro=lugar.id)
+    if id in range(Codes.COMUNA, Codes.BARRIO):
+        tangaras = ComunaCRUD.read_tangaras(db, id_comuna=id)
+    if id in range(Codes.BARRIO, Codes.VEREDA):
+        tangaras = BarrioCRUD.read_tangaras(db, id_barrio=id)
+    if id in range(Codes.VEREDA, Codes.SECTOR):
+        tangaras = VeredaCRUD.read_tangaras(db, id_vereda=id)
+    if id in range(Codes.SECTOR, Codes.AREAEXP):
+        tangaras = SectorCRUD.read_tangaras(db, id_sector=id)
+    if id in range(Codes.AREAEXP, Codes.AREAPRO):
+        tangaras = AreaExpCRUD.read_tangaras(db, id_areaexp=id)
+    if id in range(Codes.AREAPRO, Codes.AREAPRO + 1000):
+        tangaras = AreaProCRUD.read_tangaras(db, id_areapro=id)
 
-    print(tangaras)
-
-    pm25_realtime = PM25Schema(pm25=12.4, aqi=30, aqi_color="Green", aqi_category="Healthy", aqi_desc="Healthy day", datetime="now")
+    mac_addresses: list[str] = [tangara.mac for tangara in tangaras]
+    # Check data
+    if len(mac_addresses) == 0:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Tangaras Not Found")
     
-    return pm25_realtime
+    return await pm25_realtime(mac_addresses)
--- app/tests/test_pm25.py
+++ app/tests/test_pm25.py
@@ -1,13 +1,13 @@
 from fastapi.testclient import TestClient
 from fastapi import status
+import pandas as pd
 import random
 from faker import Faker
 
 from app.main import app, get_db
 from app.dependencies.testing_database import override_get_db
 
-from app.schemas.barrio import BarrioSchemaList, BarrioSchema, BarrioCreate
-from app.schemas.tangara import TangaraSchemaList
+from app.schemas.pm25 import PM25Schema
 
 
 fake = Faker()
@@ -19,4 +19,31 @@ client = TestClient(app)
 
 
 def test_get_realtime(pm25):
-    pass
+    response = client.get("/lugares/")
+    lugares = response.json()
+
+    df_lugares = pd.DataFrame(data=lugares)
+    ids_lugares = df_lugares['id'].astype('int').values
+    id_lugar = random.choice(ids_lugares)
+
+    response = client.get(f"/pm25/{id_lugar}")
+    #print("response.json():", response.json())
+    pm25realtime = response.json()
+
+    if response.status_code == status.HTTP_404_NOT_FOUND:
+        assert pm25realtime['detail'] in ["Tangaras Data Not Found", "Tangaras Not Found"]
+    else:
+        assert response.status_code == status.HTTP_200_OK
+        pm25realtime = PM25Schema.validate(pm25realtime)
+        assert pm25realtime.pm25 >= 0
+        assert pm25realtime.aqi >= 0
+        assert pm25realtime.aqi_color in ['#00e400', '#ffff00', '#ff7e00', '#ff0000', '#8f3f97', '#7e0023']
+        assert pm25realtime.aqi_category in ['Good', 'Moderate', 'Unhealthy for sensitive groups', 'Unhealthy', 'Very Unhealthy', 'Hazardous']
+        assert pm25realtime.aqi_desc in [
+            'Air quality is satisfactory, and air pollution poses little or no risk.', 
+            'Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.', 
+            'Members of sensitive groups may experience health effects. The general public is less likely to be affected.', 
+            'Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.', 
+            'Health alert: The risk of health effects is increased for everyone.', 
+            'Health warning of emergency conditions: everyone is more likely to be affected.'
+        ]
