aef7df5e6e2f75b979edb388267ea34ee6f705fa
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2023-06-16 22:57:26 -0700

    InMemory Cache Added to Lugares

--- app/routers/lugares.py
+++ app/routers/lugares.py
@@ -4,6 +4,10 @@ from sqlalchemy.orm import Session
 import pandas as pd
 import io
 
+from fastapi_cache import FastAPICache
+from fastapi_cache.decorator import cache
+from datetime import timedelta
+
 from app.dependencies.database import get_db
 from app.schemas.lugares import LugaresSchema
 from app.crud.lugares import LugaresCRUD
@@ -19,7 +23,10 @@ router = APIRouter(
 
 @router.get("/", response_model=list[LugaresSchema], status_code=status.HTTP_200_OK)
 async def lugares(format: str = "json", db: Session = Depends(get_db)) -> list[LugaresSchema]:
-    lugares = LugaresCRUD.read_lugares(db)
+    lugares = await FastAPICache.get_backend().get("lugares")
+    if not lugares:
+        lugares = LugaresCRUD.read_lugares(db)
+        await FastAPICache.get_backend().set("lugares", lugares, expire=timedelta(days=30).seconds)
     if format == "csv":
         df = pd.DataFrame(data=lugares)
         stream = io.StringIO()
